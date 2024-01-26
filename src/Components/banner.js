db8352989877d8a4adf1640da60c560b3dfba5a9
villarenteHannah <villarenteHannah22@gmail.com>
2024-01-26 20:55:47 +0800

    Update resume

--- src/Components/banner.js
+++ src/Components/banner.js
@@ -6,6 +6,7 @@ import ButtonComp from "./button";
 
 // images
 import BannerImg from "./../../src/assets/images/aestheticme2.png";
+import resume from "./../assets/resume_2024.pdf"
 
 
 
@@ -20,7 +21,7 @@ function Banner(){
                             <div className="banner-texts">
                                 <h1>I'm <b>Hannah Mae</b></h1>
                                 <h2>Front end developer and designer</h2>
-                                <a href="#contact"><ButtonComp buttonText="Hire Me"/></a>
+                                <a href={resume} target="_blank"><ButtonComp buttonText="Hire Me"/></a>
                             </div>
                         </Col>
                         <Col lg="6" md="6" sm="12">
--- src/ProjectsComponent/wordpress.js
+++ src/ProjectsComponent/wordpress.js
@@ -6,6 +6,8 @@ import ButtonComp from "./../../src/Components/button";
 // images
 import KJ from "./../../src/assets/images/kj.jpg";
 import TNB from "./../../src/assets/images/tnb.jpg";
+import stemCord from "./../../src/assets/images/stemCord.jpg";
+import ThinkTeach from "./../../src/assets/images/ThinkTeach.jpg";
 
 function Wordpress(){
     return(
@@ -38,6 +40,61 @@ function Wordpress(){
                         <div className="project-about">
                             <p>TnB provides a suite of packages designed to help entrepreneurs start their businesses in Singapore irrespective of their location. This website is made of HTML and CSS for front end, and PHP wordpress for backend.</p>
                             <a href="https://qbo.tnbgo.com/" target="_blank"><ButtonComp buttonText="View Project"/></a>
+                        </div>
+                    </div>
+                </div>
+            </Col>
+            <Col lg="4" md="6" sm="12">
+                <div className="project-box">
+                    <div className="project-image">
+                    <img
+                        src={stemCord}
+                        alt="StemCord"
+                    />
+                    </div>
+                    <div className="project-title">
+                    <p>StemCord</p>
+                    </div>
+                    <div className="project-details">
+                    <div className="project-about">
+                        <p>
+                        StemCord is an autologous, private Cord Blood Bank that focuses on
+                        processing and preserving your baby’s cord blood stem cells. This
+                        website is made of HTML and CSS for front end, and PHP wordpress for
+                        backend.
+                        </p>
+                        <a href="http://www.stemcord.com/" target="_blank">
+                        <button type="button" className="all-btn btn btn-primary">
+                            View Project
+                        </button>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+            </Col>
+            <Col lg="4" md="6" sm="12">
+                <div className="project-box">
+                    <div className="project-image">
+                        <img
+                        src={ThinkTeach}
+                        alt="Thinkteach"
+                        />
+                    </div>
+                    <div className="project-title">
+                        <p>Think Teach Academy</p>
+                    </div>
+                    <div className="project-details">
+                        <div className="project-about">
+                        <p>
+                            A top-tier education academy that embodies the core philosophy of
+                            teaching and thinking smart. This website is made of HTML and CSS for
+                            front end, and PHP wordpress for backend.
+                        </p>
+                        <a href="https://thinkteachacademy.com/dhp-2022/" target="_blank">
+                            <button type="button" className="all-btn btn btn-primary">
+                            View Project
+                            </button>
+                        </a>
                         </div>
                     </div>
                 </div>
